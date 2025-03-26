6aac54adb95771ce61c658d4646404b45c94248f
Rabek009 <cyprianlele@gmail.com>
2025-03-26 21:06:30 +0100

    crash fixed I think

--- Patches/DleksPatch.cs
+++ Patches/DleksPatch.cs
@@ -1,14 +1,36 @@
 using HarmonyLib;
+using UnityEngine;
 
 namespace MoreGamemodes
 {
-    [HarmonyPatch(typeof(AprilFoolsMode), nameof(AprilFoolsMode.ShouldFlipSkeld))]
-    class ShouldFlipSkeldPatch
+    // https://github.com/EnhancedNetwork/TownofHost-Enhanced/blob/SupportAU_2025.3.25/Patches/DleksPatch.cs
+    [HarmonyPatch(typeof(AmongUsClient._CoStartGameHost_d__37), nameof(AmongUsClient._CoStartGameHost_d__37.MoveNext))]
+    public static class DleksPatch
     {
-        public static bool Prefix(ref bool __result)
+        private static bool Prefix(AmongUsClient._CoStartGameHost_d__37 __instance, ref bool __result)
         {
-            __result = GameOptionsManager.Instance.CurrentGameOptions.MapId == 3;
+            if (__instance.__1__state != 0)
+            {
+                return true;
+            }
+            __instance.__1__state = -1;
+            if (LobbyBehaviour.Instance)
+            {
+                LobbyBehaviour.Instance.Despawn();
+            }
+            if (ShipStatus.Instance)
+            {
+                __instance.__2__current = null;
+                __instance.__1__state = 2;
+                __result = true;
+                return false;
+            }
+
+            var num2 = Mathf.Clamp(GameOptionsManager.Instance.CurrentGameOptions.MapId, 0, Constants.MapNames.Length - 1);
+            __instance.__2__current = __instance.__4__this.ShipLoadingAsyncHandle = __instance.__4__this.ShipPrefabs[num2].InstantiateAsync();
+            __instance.__1__state = 1;
+            __result = true;
             return false;
-        }
+    }
     }
 }
